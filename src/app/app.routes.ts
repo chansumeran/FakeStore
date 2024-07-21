1cdd2f4b47e5d40ae4f7b697786384abf216ad88
Christian <rchristiansumeran@gmail.com>
2024-07-21 17:24:32 +0800

    refactored code inconsistency

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
-import {AppComponent} from "./app.component";
 import {HomeComponent} from "./components/home/home.component";
+import {ProductComponent} from "./components/product/product.component";
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'products',
+    component: ProductComponent
   }
 ];
